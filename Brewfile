b224ac4a7b1e8ed5c836c7f27f7b1a3c29766ba1
Uche Chilaka <uche@beam.dental>
2021-04-08 00:40:49 -0400

    feat: made variables for scripts/mongo-express.sh dynamic

---
+++ Brewfile
@@ -0,0 +1,4 @@
+# @CAUTION You only need to run `brew install`
+#   if you don't have the realpath util available locally
+brew 'coreutils'
+tap 'homebrew/dupes'
--- scripts/mongo-express.sh
+++ scripts/mongo-express.sh
@@ -2,9 +2,19 @@
 # @IMPORTANT requires mongo.simple-app container!
 # @IMPORTANT requires .env.local exists and sets DB env variables!
 
+REPO_ROOT=$(realpath "$(dirname ${BASH_SOURCE[0]})/../")
+
+ENV_FILE="${REPO_ROOT}/.env"
+ENV_LOCAL_FILE="${REPO_ROOT}/.env.local"
+
+# Load environment variables from file(s)
+[ -f $ENV_FILE ] && export $(egrep -v '^#' $ENV_FILE | xargs)
+[ -f $ENV_LOCAL_FILE ] && export $(egrep -v '^#' $ENV_LOCAL_FILE | xargs)
+
 docker run -it --rm \
 --name mongo-express.simple-app \
 --network simple-app-network \
 --link "mongo.simple-app:mongo" \
---env-file .env.local \
+-e ME_CONFIG_MONGODB_ADMINUSERNAME="${DATABASE_USERNAME}" \
+-e ME_CONFIG_MONGODB_ADMINPASSWORD="${DATABASE_PASSWORD}" \
 -p 8081:8081 mongo-express
